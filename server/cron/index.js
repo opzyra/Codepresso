8d46820fffb2acb70442cd88ebd70f9858efd395
opzyra <opzyra@gmail.com>
2019-01-28 00:16:10 +0900

    dev-prerender 크론탭 추가

--- server/cron/index.js
+++ server/cron/index.js
@@ -1,6 +1,5 @@
 const cron = require('node-cron')
-const TopicFeed = require('../models/topic_feed')
-
+const { sequelize } = require('../models')
 const fs = require('fs')
 
 cron.schedule('0 0 23 * * *', async () => {
@@ -10,8 +9,8 @@ cron.schedule('0 0 23 * * *', async () => {
       where tf.feedIdx = f.idx
       AND tf.topicIdx = :idx
     `
-  const result = await TopicFeed.sequelize.query(query, { replacements: { idx: 2 } })
-  const feeds = result[0]
+  const qrs = await sequelize.query(query, { replacements: { idx: 2 } })
+  const feeds = qrs[0][0]
 
   feeds.forEach(el => {
     fs.writeFile(`/opt/github/TIL/${el.title}.md`, `# ${el.title}\n` + `${el.contents}`, 'utf-8', err => {
@@ -22,6 +21,36 @@ cron.schedule('0 0 23 * * *', async () => {
         console.log(`/opt/github/TIL/${el.title}.md is WRITE DONE!`)
       }
     })
+  })
+
+})
+
+cron.schedule('0 30 0 * * *', async () => {
+  let query = `
+      SELECT
+        (SELECT COUNT(*) FROM topic) AS topic,
+        (SELECT COUNT(*) FROM feed) AS feed,
+        (SELECT COUNT(*) FROM tag) AS tag,
+        (SELECT COUNT(*) FROM comment) AS comment
+      FROM DUAL
+    `
+  const qrs = await sequelize.query(query)
+  const counts = qrs[0][0]
+  let routes = []
+
+  for (let i = 1; i <= counts.topic; i++) {
+    routes.push(`/devlog/topic/${i}`)
+  }
+  for (let i = 1; i <= counts.feed; i++) {
+    routes.push(`/devlog/feed/${i}`)
+  }
+  fs.writeFile(`/opt/client/prerender.config.js`, `module.exports = ['${routes.join('\', \'')}']`, 'utf-8', err => {
+    // TODO Log를 남기자.. 시스템에서 잘 처리됬는지 기록을 남겨야 알 수 있다.
+    if(err){
+      console.log(err)
+    }else{
+      console.log(`DONE!`)
+    }
   })
 
 })
